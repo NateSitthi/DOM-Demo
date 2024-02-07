b2bbb2b8683e00119383d501dc06cd15c000c027
NateSitthi <143848445+NateSitthi@users.noreply.github.com>
2024-02-07 14:14:05 -0800

    ads

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 // Add Event listners when DOM is ready
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded"), function () {
 
     // check for buttons exist
     if ( document.querySelectorAll('.button') !== null) {
         let buttons = document.querySelectorAll('.button');
         buttons.forEach(button => button.addEventListener('click', processForm))
     }
-
+  }
     // optionally update when fields update
-    // tbd
+    document.getElementById('name').addEventListener('change', function (eventData) {
+      let nameOutput = document.getElementById('nameOutput');
+      console.log(eventData);
+      nameOutput.innerText = eventData.target.value;
+    });
+    document.getElementById('type').addEventListener('change', function (eventData) {
+      let nameOutput = document.getElementById('nameOutput');
+      let info = document.getElementById('info');
+      nameOutput.innerText = formData.name;
+      info.innerText = formData.type;
+    })
 
     // process form Data
-    document.getElementById("myForm").addEventListener("submit", function (e) {
-        e.preventDefault();
-      
-        var formData = new FormData(form);
+    document.getElementById("myForm").addEventListener("submit", function (eventData) {
+        eventData.preventDefault();
+        console.log(eventData.target);
+        var formData = new FormData(eventData.target);
+        formData = Object.fromEntries(formData);
         // output as an object
         console.log(Object.fromEntries(formData));
       
@@ -22,8 +33,13 @@ document.addEventListener("DOMContentLoaded", function () {
         for (var pair of formData.entries()) {
           console.log(pair[0] + ": " + pair[1]);
         }
-      });
+      // });
     // Log readiness to console
+      let nameOutput = document.getElementById('nameOutput');
+      let info = document.getElementById('info');
+      nameOutput.innerText = formData.name;
+      info.innerText = formData.type;
+
     console.log("Ready");
 });
 
